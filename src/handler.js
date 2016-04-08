63dc17160163b27de79ec3ef9c0aa4c041b28704
James Wyatt Cready <wyatt.cready@lanetix.com>
2016-04-08 13:22:18 -0400

    Change params

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,4 @@
-export function handler ({ payload }, request, done) {
+export function handler ({ payload, request }, done) {
   const { record: { id, apiName }, priorState, changeSet } = payload
   const wasNameChanged = 'first_name' in changeSet || 'last_name' in changeSet
   if (!wasNameChanged) return done(null, 'No name change')
